f2a0952c78140d76974c7321d9cabf6dbec5a15d
Roman <romantcvetkov@gmail.com>
2020-09-26 17:10:50 +0300

    Updates

---
+++ precache-manifest.23a269176423847f560c7c9cd91351a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8dc845cbc118aaa6a741f83f0ef28c",
+    "url": "/react-hw-16/index.html"
+  },
+  {
+    "revision": "5b13100b85c9a0820c86",
+    "url": "/react-hw-16/static/css/main.cc87cf3d.chunk.css"
+  },
+  {
+    "revision": "e10a647534f74669ae7e",
+    "url": "/react-hw-16/static/js/2.859695b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-16/static/js/2.859695b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b13100b85c9a0820c86",
+    "url": "/react-hw-16/static/js/main.9e861652.chunk.js"
+  },
+  {
+    "revision": "2fa01442ad941728ba97",
+    "url": "/react-hw-16/static/js/runtime-main.af7049a0.js"
+  },
+  {
+    "revision": "e5c281244563a10a71d6518ce6dff3ce",
+    "url": "/react-hw-16/static/media/anonymous.e5c28124.svg"
+  },
+  {
+    "revision": "07ea4eabdf04eb6b034049d4eb99f812",
+    "url": "/react-hw-16/static/media/female.07ea4eab.svg"
+  },
+  {
+    "revision": "037e22c95230161ce14cf0fe8872b9b4",
+    "url": "/react-hw-16/static/media/male.037e22c9.svg"
+  }
+]);
